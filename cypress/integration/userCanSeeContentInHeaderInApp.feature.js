1fb87f944dc543c7f67272baf6fe7668e2cb2b0a
maxarvid <max.arvid.anderson@gmail.com>
2021-09-13 14:26:18 +0200

    adds api data, tasting fixture, additional tests

--- cypress/integration/userCanSeeContentInHeaderInApp.feature.js
+++ cypress/integration/userCanSeeContentInHeaderInApp.feature.js
@@ -1,9 +1,23 @@
 describe("User can see the Header in App.jsx", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000");
+    cy.intercept("GET", "*/users**", { fixture: "users.json" });
+    cy.visit("/");
   });
 
   it("is expected to have text in the header", () => {
     cy.get("h1").should("contain", "Employee Management");
+  });
+
+  describe("emplpyee list section", () => {
+    it("is expected to display 4 employees", () => {
+      cy.get("[data-testid=employee-list]").children().should("have.length", 4);
+    });
+
+    it("is expected to return Thomas as first employee in list", () => {
+      cy.get("[data-testid=employee-list]")
+        .children()
+        .first()
+        .should("contain.text", "Thomas Bluth");
+    });
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [employees, setEmployees] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get("https://reqres.in/api/users?per_page=4");
+    setEmployees(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const employeeList = employees.map((employee) => {
+    return <li>{`${employee.first_name} ${employee.last_name}`}</li>;
+  });
+
   return (
     <div>
-      <h1>Employee Management</h1>
+      <h1 data-testid="header">Employee Management</h1>
+      <div data-testid="employee-list">{employeeList}</div>
     </div>
   );
 };
